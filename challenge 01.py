d1e627ab3e46b68b40cd2d87f87f6febaff61f45
Kleber Jr. <kleberwagner107@gmail.com>
2025-04-03 15:44:50 -0300

    consegui resolver o problema proposto pelo professor, está funcionando perfeitamente

--- challenge 01.py
+++ challenge 01.py
@@ -1,16 +1,36 @@
-entrada01hr = 21
-entrada01min = 30
-entrada02hr = 21
-entrada02min = 30
+entrada01hr = int(input("Digite a primeira hora: "))
+entrada01min = int(input("Digite o primeiro minuto: "))
+entrada02hr = int(input("Digite a segunda hora: "))
+entrada02min = int(input("Digite o segundo minuto: "))
 hr1min = entrada01hr*60
 hr2min = entrada02hr*60
 mintotal = (hr1min+hr2min+entrada01min+entrada02min)
 hrstotal = (mintotal/60)
 hrsint = (mintotal//60)
 hrfinal = hrsint-12
-teste = (hrstotal-hrsint)
-teste2 = teste*60
-if hrfinal > 12:
-    print(f"{hrfinal-12}:{teste2:.0f}")
+mindec = (hrstotal-hrsint)
+minfinal = mindec*60
+if hrfinal >= 12:
+    hrfinal = hrfinal-12
 else:
-    print(f"{hrfinal}:{teste2:.0f}")
+    print(f"{hrfinal}:{minfinal:.0f}h")
+if hrfinal >= 12:
+    hrfinal = hrfinal-12
+else:
+    print(f"{hrfinal}:{minfinal:.0f}h")
+if hrfinal >= 12:
+    hrfinal = hrfinal-12
+else:
+    print(f"{hrfinal}:{minfinal:.0f}h")
+
+
+print(f"{hr1min} hr1min")
+print(f"{hr2min} hr2min")
+print(f"{mintotal} mintotal")
+print(f"{hrstotal} hrstotal")
+print(f"{hrsint} hrsint")
+print(f"{hrfinal} hrfinal")
+print(f"{mindec} teste")
+print(f"{minfinal:.0f} teste2")
+print(" ")
+print(f"{hrfinal}:{minfinal:.0f}h")
