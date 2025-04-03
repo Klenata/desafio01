891349ef81c459d880553f6ab2cad24b66429962
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-03 11:26:13 -0300

    criei o arquivo 'desafio01' e desenvolvi o sistema para identificar somar dois horarios e transformar em horario militar

---
+++ challenge 01.py
@@ -0,0 +1,16 @@
+entrada01hr = 21
+entrada01min = 30
+entrada02hr = 21
+entrada02min = 30
+hr1min = entrada01hr*60
+hr2min = entrada02hr*60
+mintotal = (hr1min+hr2min+entrada01min+entrada02min)
+hrstotal = (mintotal/60)
+hrsint = (mintotal//60)
+hrfinal = hrsint-12
+teste = (hrstotal-hrsint)
+teste2 = teste*60
+if hrfinal > 12:
+    print(f"{hrfinal-12}:{teste2:.0f}")
+else:
+    print(f"{hrfinal}:{teste2:.0f}")
